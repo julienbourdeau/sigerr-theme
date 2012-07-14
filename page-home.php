4097059f3e2f30c33851b7a35d6c4daf4f104eb6
Julien Bourdeau <julien@sigerr.org>
2012-07-15 00:49:26 +0200

    home page started

--- page-home.php
+++ page-home.php
@@ -19,11 +19,30 @@ get_header(); ?>
 		<div id="primary" class="site-content span12 full-width">
 			<div id="content" role="main">
 
-				<?php while ( have_posts() ) : the_post(); ?>
+				<div class="row">
+					<div class="span6">
+						<h2>All my recent posts</h2>
+						<?php ypfwp_display_yahoo_pipe(
+									"http://pipes.yahoo.com/pipes/pipe.run?_id=be111f264562dade4584cd4185a64b73&_render=json",
+									86400,
+									"my-yahoo-pipe",
+									20
 
-					here goes the content
+							); ?>
+					</div>
 
-				<?php endwhile; // end of the loop. ?>
+					<div class="span6">
+
+						<h2>My projects</h2>
+						<?php //the_widget(''); ?>
+
+						<?php while ( have_posts() ) : the_post(); ?>
+							<h2><?php the_title(); ?></h2>
+							<?php the_content(); ?>
+						<?php endwhile; // end of the loop. ?>
+
+					</div>
+				</div>
 
 			</div><!-- #content -->
 		</div><!-- #primary .site-content -->
