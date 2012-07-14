b3b6b23575c35fd7a05c83a5714d51aecf2a67f9
Julien Bourdeau <julien@sigerr.org>
2012-07-14 22:48:55 +0200

    modified a bit entry-metas

--- content-single.php
+++ content-single.php
@@ -24,6 +24,8 @@
 
 	<footer class="entry-meta">
 		<?php
+			sigerr_posted_on();
+
 			/* translators: used between list items, there is a space after the comma */
 			$category_list = get_the_category_list( __( ', ', 'sigerr' ) );
 
