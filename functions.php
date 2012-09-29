68aaa590f1a0f5b5b8fefd004acb2716d856f4a1
Julien Bourdeau <julien@sigerr.org>
2012-09-29 08:33:56 +0200

    removed many includes in functions.php - loads less code

--- functions.php
+++ functions.php
@@ -77,7 +77,7 @@ function sigerr_setup() {
 	/**
 	 * Custom Theme Options
 	 */
-	require( get_template_directory() . '/inc/theme-options/theme-options.php' );
+	//require( get_template_directory() . '/inc/theme-options/theme-options.php' );
 
 	/**
 	 * WordPress.com-specific functions and definitions
@@ -90,7 +90,7 @@ function sigerr_setup() {
 	 * If you're building a theme based on _s, use a find and replace
 	 * to change 'sigerr' to the name of your theme in all the template files
 	 */
-	load_theme_textdomain( 'sigerr', get_template_directory() . '/languages' );
+	//load_theme_textdomain( 'sigerr', get_template_directory() . '/languages' );
 
 	/**
 	 * Add default posts and comments RSS feed links to head
@@ -125,7 +125,7 @@ function sigerr_setup() {
 	/**
 	 * Add support for the Aside Post Formats
 	 */
-	add_theme_support( 'post-formats', array( 'aside', ) );
+	//add_theme_support( 'post-formats', array( 'aside', ) );
 }
 endif; // sigerr_setup
 add_action( 'after_setup_theme', 'sigerr_setup' );
@@ -159,19 +159,19 @@ function sigerr_scripts() {
 		wp_enqueue_style( 'style-dev', get_template_directory_uri() . '/css/style-'.ENV.'.css', array( 'style' ) );
 	}
 
-	wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
+	//wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
 
 	if ( defined('ENV') && !is_admin() ) {
 		wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap-'.ENV.'.min.js', array( 'jquery' ), '2.0.4', true );
 	}
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+	//if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+	//	wp_enqueue_script( 'comment-reply' );
+	//}
 
-	if ( is_singular() && wp_attachment_is_image( $post->ID ) ) {
-		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
+	//if ( is_singular() && wp_attachment_is_image( $post->ID ) ) {
+	//	wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+	//}
 }
 add_action( 'wp_enqueue_scripts', 'sigerr_scripts' );
 
