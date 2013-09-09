e953bbf1703c1aa361defc855cd2271118103070
Julien Bourdeau <julien@sigerr.org>
2013-09-09 07:46:39 +0200

    centered the whole header

--- header.php
+++ header.php
@@ -39,26 +39,27 @@
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
 		<div class="container">
-			<div class="row">
 
-				<div class="span4">
+			<div class="row">
+				<div class="span12">
 					<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				</div>
+			</div>
 
-				<div class="span8">
+			<div class="row">
+				<div class="span12">
 					<nav role="navigation" class="site-navigation main-navigation">
 						<h1 class="assistive-text"><?php _e( 'Menu', 'sigerr' ); ?></h1>
 						<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'sigerr' ); ?>"><?php _e( 'Skip to content', 'sigerr' ); ?></a></div>
 
 						<?php wp_nav_menu( 
 											array( 	'theme_location' 	=> 'primary',
-													'menu_class'		=> 'nav nav-pills pull-right',
+													'menu_class'		=> 'nav nav-pills',
 													'container_class' => 'menu-access-container hidden-phone',
 												 )
 										 ); ?>
 					</nav>
 				</div>
-
 			</div>
 
 		</div>
